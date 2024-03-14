8f50d855dd09663550b74f8cd7bdbe72d4911b84
atharvaraut0 <atharvaraut99@gmail.com>
2024-03-13 20:06:23 -0700

    adding extra comments

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ console.log('hello');
 
 // randomizer
 
-const buttons = document.getElementsByClassName('clicker');
+const buttons = document.getElementsByClassName('clicker'); //Getting the buttons as an array
 // console.log(buttons);  //CHECK
 
-
+//run iterations for the amount of elements in out buttons array
 for(let i=0; i < buttons.length; i++) {
-    let j = Math.round(Math.random(i)); // assign a random number between 0 and 1
+    let j = Math.round(Math.random(i)); // assign either 0 or 1 based on the array index
     
-    if(j%2 == 0) {
+    //Check weather number is "even" or "odd"
+    if(j%2 == 0) { 
         buttons[i].style.backgroundColor = 'red';
     
     } else {
